1d9540edd4d3aff3da5d113ee9e887c3bc16c33e
Shruti Bokil <shrutibokil2005@gmail.com>
2026-05-26 10:51:34 +0530

    i18n: Add Hindi translation

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing);
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(ta|en|bn|te)/:path*']
+  matcher: ['/', '/(ta|en|hi|bn|te)/:path*']
 };
